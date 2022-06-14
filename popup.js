7d013e06c8f6ddd859e59a79be84184b49b41ad0
weronikadominiak <weronika.dominiak@hotmail.com>
2022-06-14 20:41:39 +0100

    refactor: cleanup the code, update readme

--- popup.js
+++ popup.js
@@ -12,44 +12,48 @@ generateCalendarBtn.addEventListener("click", async () => {
 });
 
 
-function generateCalendar() {
+const generateCalendar = () => {
+    // helpers
     const generateGoogleLink = (year, month, day, hour, minute, endHour) => `http://www.google.com/calendar/event?action=TEMPLATE&dates=${year}${month}${day}T${hour}${minute}00Z%2F${year}${month}${day}T${endHour}${minute}00&text=Swimming&location=&details=`
+    const formatDateElement = (element) => element < 10 ? `0${element}` : element;
 
-
-    // Define constants
+    // Constants
     const MONTHS_MAP = {
-        "Jan": "01",
-        "Feb": "02",
-        "Mar": "03",
-        "Apr": "04",
-        "May": "05",
-        "Jun": "06",
-        "Jul": "07",
-        "Aug": "08",
-        "Sep": "09",
-        "Oct": "10",
-        "Nov": "11",
-        "Dec": "12"
+    "Jan": "01",
+    "Feb": "02",
+    "Mar": "03",
+    "Apr": "04",
+    "May": "05",
+    "Jun": "06",
+    "Jul": "07",
+    "Aug": "08",
+    "Sep": "09",
+    "Oct": "10",
+    "Nov": "11",
+    "Dec": "12"
     }
     const DAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
-    // Check first day of current month
-    const currentMonth = new Date().getMonth();
-    const firstDayThisMonth = new Date(new Date().getFullYear(), currentMonth, 1).getDay();
+
+    // CALENDAR GENERATION
+
 
     // Generate a list of dates for the current month
     const allBookings = Array.from(document.querySelectorAll("tr"));
-
     const bookedDays = allBookings.filter(booking => booking.cells[1].localName  === "td").map(booking => {
         const date = booking.cells[1].innerText.split(" ");
         const year = date[2];
         const month = MONTHS_MAP[date[1]];
-        const day = date[0].length === 1 ? `0${date[0]}` : date[0];
+        const day = formatDateElement(date[0]);
         const time = date[3];
         const dateString =  `${year}-${month}-${day}T${time}:00`;
 
         return new Date(dateString);
     });
+
+        // Check first day of current month 
+    const currentMonth = new Date().getMonth();
+    const firstDayThisMonth = new Date(new Date().getFullYear(), currentMonth, 1).getDay();
 
     // Generate a list of dates for the current month sorted in ascending order
     const bookingsThisMonth = bookedDays.filter(day => day.getMonth() === currentMonth).sort((a,b) => Date.parse(a) - Date.parse(b));
@@ -86,20 +90,19 @@ function generateCalendar() {
         const cell = calendar.querySelector(`[data-day="${day}"]`);
         cell.classList.add("booked");
   
+        // display booking time
         const hour = booking.getHours();
-        const formattedHour = hour < 10 ? `0${hour}` : hour;
-        const formattedEndHour = hour + 1 < 10 ? `0${hour +1}` : hour +1;
-        const minutes = booking.getMinutes();
-        const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+        const formattedHour = formatDateElement(hour);
+        const formattedEndHour = formatDateElement(hour + 1);
+        const formattedMinutes = formatDateElement(booking.getMinutes());
         cell.innerHTML += `<p>${formattedHour}:${formattedMinutes}</p>`;
 
         // generate button with Google Calendar link
         const button = document.createElement("a");
         button.classList.add("add-to-calendar");
         button.innerText = "Add to calendar";
-        const formattedDay = day < 10 ? `0${day}` : day;
-        const correctedMonth = booking.getMonth() + 1; // get month counts from 0-index
-        const formattedMonth = correctedMonth +1 < 10 ? `0${correctedMonth}` : correctedMonth;
+        const formattedDay = formatDateElement(day);
+        const formattedMonth = formatDateElement(booking.getMonth() + 1); // get month counts from 0-index
         button.href = generateGoogleLink(booking.getFullYear(), formattedMonth, formattedDay, formattedHour, formattedMinutes, formattedEndHour);
         
         cell.appendChild(button);
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Places Leisure Chrome Extension
+
+## What is it?
+
+My local swimming pool's website doesn't show your bookings in the calendar view and by default displays bookings in filtered by booking date, I decided to change that. This simple chrome extension will quickly generate a calendar view on top of the website listing all of your bookings for the current month with the option to add a Google Calendar event.
+
+![image description](demo.gif)
+
+## How can I use it?
+
+1. Clone this repository
+2. Follow this [official guide]("https://developer.chrome.com/docs/extensions/mv3/getstarted/#unpacked") from Google Developer Documentation.
+
+## See it in action
